fec27c33a575cd6f324b3a2f9b447527e16d27f4
Pengfei Zhang <pzhang11@stevens.edu>
2014-11-06 10:59:12 -0500

    add regist.php, use DB.php-> regist($email, $name, $passwd) to check and regist.
    Signed-off-by: Pengfei Zhang <pzhang11@stevens.edu>

---
+++ include/regist.php
@@ -0,0 +1,19 @@
+<?php
+
+	include ("./DB.php");
+	
+	
+
+	if(isset($_POST["email"]) && isset($_POST["Name"]) && isset($_POST["password"])){
+		$Email = $_POST["email"];
+		$Name = $_POST["Name"];
+		$Password = $_POST["password"];
+		
+		
+	}
+	else{
+		
+	}
+	
+	
+?>
