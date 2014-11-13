864b68848ae4bd54c9003f914bf9b7ca5c5e414b
Pengfei Zhang <pzhang11@stevens.edu>
2014-11-13 16:36:55 -0500

    finish email validation with AJAX & Jquery.
    Signed-off-by: Pengfei Zhang <pzhang11@stevens.edu>

--- js/regist.js
+++ js/regist.js
@@ -1,15 +1,38 @@
 /**
  * 
  */
-  $(document).ready(function(){
-		$("input[id='confirm_password']").bind('input', function(){
-			var text = $("input[id='password']").val();
-			if(text != $("input[id='confirm_password']").val()){
-				$(this).css("background-color", "red");
-			}
-			else{
+ $(document).ready(function(){
+ 	$("input[id='confirm_password']").bind('input', function(){
+ 		var text = $("input[id='password']").val();
+ 		if(text != $("input[id='confirm_password']").val()){
+ 			$(this).css("background-color", "red");
+ 		}
+ 		else{
 
-				$(this).css("background-color", "green");
-			}
-		});
-	});
+ 			$(this).css("background-color", "green");
+ 		}
+ 	});
+ 	
+ 	$("input[id='email']").bind('blur',fn);
+ });
+ 
+ 
+
+	function fn(){
+		$.post("../include/AJAXEmailComfirm.php", {email: $(this).val()},
+				function(xml){
+				//alert(xml);
+				res = $("Validate",xml).text();
+				reason = $("Content",xml).text();
+				//alert(res);
+	 			if(res == "true"){
+	 				$("input[id='email']").css("background-color", "green");
+	 			}
+	 			else{
+	 				$("input[id='email']").val(reason);
+	 				$("input[id='email']").css("background-color", "red");
+	 			}
+
+			});
+
+	};
