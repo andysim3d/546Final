232eb9daf38983b440278b2a020c6b634ec97b88
Hang Yang <yanghang90620@gmail.com>
2014-11-18 14:37:53 -0500

    login jquery validate
    Signed-off-by: Hang Yang <yanghang90620@gmail.com>

--- js/index.js
+++ js/index.js
@@ -15,3 +15,37 @@ $(document).ready(function() {
 		});
 	});
 });
+
+// When the browser is ready...
+$(function() {
+
+  // Setup form validation on the #register-form element
+  $("#login_form").validate({
+  
+      // Specify the validation rules
+      rules: {
+          email: {
+              required: true,
+              email: true
+          },
+          password: {
+              required: true,
+          }
+      },
+      
+      // Specify the validation error messages
+      errorElement: "div",
+      messages: {
+          password: {
+              required: "Please provide a password",
+          },
+          required: "You need input your email address",
+          email: "Please enter a valid email address"
+          
+      },
+      
+      submitHandler: function(form) {
+          form.submit();
+      }
+  });
+});
