926c12cadd8664d8f4005c56337f47b0b1d7e617
Hang Yang <yanghang90620@gmail.com>
2014-11-18 17:05:26 -0500

    Signed-off-by: Hang Yang <yanghang90620@gmail.com>

--- js/regist.js
+++ js/regist.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
  	$("input[id='confirm_password']").bind('input', function(){
  		var text = $("input[id='password']").val();
  		if(text != $("input[id='confirm_password']").val()){
- 			$(this).css("background-color", "red");
+		$(this).css("background-color", "red");
  		}
  		else{
 
- 			$(this).css("background-color", "green");
+$(this).css("background-color", "green");
  		}
  	});
  	
