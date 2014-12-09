458a5590d0d51054bff3947bd1b6f311d96575ee
Hang Yang <yanghang90620@gmail.com>
2014-12-09 16:30:44 -0500

    change the files to standard nav part
    Signed-off-by: Hang Yang <yanghang90620@gmail.com>

--- pages/article.php
+++ pages/article.php
@@ -13,49 +13,8 @@
 <link href="../css/article_site.css" rel="stylesheet">
 
 </head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation"
-		id="nav_bar">
-		<div class="container">
-			<div class="navbar-header" id="navbar_header">
-				<button type="button" class="navbar-toggle collapsed"
-					data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-					aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="../pages/index.html">Answer</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><form class="navbar-form ">
-							<input type="text" class="form-control" placeholder="Search...">
-							<button type="submit" class="btn btn-primary">Search</button>
-						</form></li>
-					<li><a href="../pages/index.php">Home</a></li>
-					<li class="active"><a href="../pages/article.php">Article</a></li>
-					<li class="dropdown"><a href="#" class="dropdown-toggle"
-						data-toggle="dropdown">Settings <span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="../pages/profile.php">Profile</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li class="dropdown-header">Nav header</li>
-							<li><a href="#">Separated link</a></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li><button class="btn btn-primary">LOGIN</button></li>
-					<li><button class="btn btn-primary" onclick="newDoc()">Sign Up</button></li>
-				</ul>
-			</div>
-			<!--/.nav-collapse -->
-		</div>
-	</nav>
 
+<?PHP  include '../include/page_title.php'; ?>
 	<div class="container">
 
 		<!-- Main component for a primary marketing message or call to action -->
--- pages/postquestion.php
+++ pages/postquestion.php
@@ -7,97 +7,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>Index of Answer</title>
+<title>Post Question</title>
 
 <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" rel="stylesheet">
 <link href="../css/bootstrap.min.css" rel="stylesheet">
 <link href="../css/index_site.css" rel="stylesheet">
 
 </head>
-<body>
-	<nav class="navbar navbar-default navbar-fixed-top" role="navigation"
-		id="nav_bar">
-		<div class="container">
-			<div class="navbar-header" id="navbar_header">
-				<button type="button" class="navbar-toggle collapsed"
-					data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-					aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span> <span
-						class="icon-bar"></span> <span class="icon-bar"></span> <span
-						class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="../pages/index.php">Q&A</a>
-			</div>
-			<div id="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					<li><form class="navbar-form ">
-							<input type="text" class="form-control" placeholder="Search..." >
-							<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>Search</button>
-						</form></li>
-					<li><a href="../pages/index.php">Home</a></li>
-					<li><a href="../pages/article.php">Article</a></li>
-					<li class="dropdown"><a href="#" class="dropdown-toggle"
-						data-toggle="dropdown">Settings <span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="../pages/profile.php">Profile</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li class="dropdown-header">Nav header</li>
-							<li><a href="#">Separated link</a></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul></li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-				
-					<li>
-					
-					
-					<?php 
-					if(isset($_SESSION['UID'])){
-						echo "<button class=\"btn btn-primary\" id=\"login_button\">";
-						echo $_SESSION['Name'];
-					}
-					else{
-						echo "<button class=\"btn btn-primary\" id=\"login_button\">"; 
-						echo "Login";
-					}
-					?>
-					</button></li>
-					
-					<li>
-					
-					
-					<?php 
-					if(isset($_SESSION['UID'])){
-						echo "<button class=\"btn btn-primary\" id=\"signup_button\">";
-						echo "Log Out";
-					}
-					else{
-						echo "<button class=\"btn btn-primary\" id=\"signup_button\">";
-						echo "Sign Up";
-					}
-					?>
-					</button>
-							</li>
-							
-					<li>
-							<?php 
-					if(isset($_SESSION['UID'])){
-						echo "<button class=\"btn btn-primary\" id=\"post_button\" onclick=\"Jump_question()\">";
-						echo "<span class=\"\glyphicon glyphicon-question-sign\" aria-hidden=\"true\"></span>";
-						echo "Add Questions";
-						echo "</button>";
-					}
-					?>
-
-							</li>
-					
-				</ul>
-			</div>
-			<!--/.nav-collapse -->
-		</div>
-	</nav>
+<?PHP  include '../include/page_title.php'; ?>
 
 	<div class="container" id="dialog_container">
 		<div id="dialog" title="LOGIN">
