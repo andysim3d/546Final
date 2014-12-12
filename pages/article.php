7fa147a6a4432c3527f1760852b2819c368f1ff3
Hang Yang <yanghang90620@gmail.com>
2014-12-12 16:08:41 -0500

    update article.php @pages and implement basic functions
    Signed-off-by: Hang Yang <yanghang90620@gmail.com>

--- pages/article.php
+++ pages/article.php
@@ -14,31 +14,81 @@
 
 </head>
 
-<?PHP  include '../include/page_title.php'; ?>
+<?PHP  include '../include/page_title.php';?>
+
+	<div class="container" id="dialog_container">
+		<div id="dialog" title="LOGIN">
+			<form action="../include/AJAXLOGIN.php" method="post" id="login_form" novalidate="novalidate">
+				<div class="form-group">
+					<div class="row">
+						<div class="col-md-4">
+							<label>Email:</label> <input id="email" name="email" type="text">
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="row">
+						<div class="col-md-4">
+							<label>Password:</label> 
+
+							<input id="password" name="password"
+								type="password"/>
+								 <button id="LoginBtn" type="Button"
+								value="Login">Login 	</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<!-- /container -->
 	<div class="container">
 
-		<!-- Main component for a primary marketing message or call to action -->
-		<div class="jumbotron">
-			<h1>Navbar example</h1>
-			<p>This example is a quick exercise to illustrate how the
-				default, static and fixed to top navbar work. It includes the
-				responsive CSS and HTML, so it also adapts to your viewport and
-				device.</p>
-			<p>To see the difference between static and fixed top navbars,
-				just scroll.</p>
-			<p>
-				<a class="btn btn-lg btn-primary" href="../../components/#navbar"
-					role="button">View navbar docs &raquo;</a>
-			</p>
-		</div>
+		<fieldset>
+			<legend>Articles</legend>
+<?PHP
+include("../include/DB.php");
+$db=new database();
+$db->connect();
+$query_article="SELECT user.Name,article.*,user.UID
+                FROM `user`
+				INNER JOIN `article`
+				ON user.UID=article.UID
+";
+ 	if(!$res_article = $db->send_sql($query_article)){
+		$db->disconnect();
+		echo "Get Questions failed!<br>\n";
+		return -1;
+	}
+	$i=0;
+	$num=mysqli_num_rows($res_article);
+	while($i<$num)
+	{
+	$content=$res_article->fetch_assoc();
+	echo "<div class=\"jumbotron\">\n";
+	echo "<h4><a href=\"article_view.php?var=".$content['ArtID']."\">".$content['Title']."</a></h4>";
+	echo "<h6>Author:".$content['Name']."</h6>\n";
+    echo "<h6>Time:".$content['Time']."</h6>\n";
+	echo "<h4></h4>";
+	echo "<span class=\"glyphicon glyphicon-thumbs-up\" aria-hidden=\"true\"></span><span class=\"count\">&nbsp".$content['Up_Vote']."</span>";
+	echo "<span>&nbsp &nbsp &nbsp</span>";
+	echo "<span class=\"glyphicon glyphicon-thumbs-down\" aria-hidden=\"true\"></span></span><span class=\"count\">&nbsp-".$content['Down_Vote']."</span>";
+	echo "</div>";
+	$i++;
+	}
+	$db->disconnect();
+?>
+		</fieldset>
 
 	</div>
-	<!-- /container -->
-
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script
 		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script
+		src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
 	<script src="../js/index.js"></script>
+	<script src="../js/jquery.validate.min.js"></script>
 </body>
 </html>
