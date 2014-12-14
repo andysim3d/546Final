e079d4b6429bc8c28d466d8c4cf594169e9b8686
Hang Yang <yanghang90620@gmail.com>
2014-12-14 16:36:58 -0500

    update edit_answer.php @ pages and implement basic functions
    Signed-off-by: Hang Yang <yanghang90620@gmail.com>

--- pages/edit_answer.php
+++ pages/edit_answer.php
@@ -1,4 +1,13 @@
-<?php session_start(); ?>
+
+<?php session_start();
+if((isset($_SESSION['login']))&&($_SESSION['login']==true))
+{
+//print_r($_SESSION);
+}
+else{
+//header("Location: http://localhost/546Final/pages/index.php");
+}
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +24,8 @@
 
 </head>
 
-<?PHP  include '../include/page_title.php'; ?>
+<?PHP  include '../include/page_title.php';?>
+
 	<div class="container" id="dialog_container">
 		<div id="dialog" title="LOGIN">
 			<form action="../include/AJAXLOGIN.php" method="post" id="login_form" novalidate="novalidate">
@@ -41,13 +51,87 @@
 			</form>
 		</div>
 	</div>
+	
+
+	
     <div class="container">
+	    	<fieldset>
+			<legend>Question</legend>
 	<?PHP
+	include("../include/DB.php");
 	if(isset($_GET['var']))
 	{
-	include("../include/");
+       $question_id=$_GET['var'];
+	   $question_content=GetQuestion_ByID($question_id);
+	  // print_r($question_content);
+		echo "<h3>".$question_content[0]['Title']."</h4>\n";
+		echo "<p>Details:".$question_content[0]['Content']."</p>\n";
+	    echo "<h6>Poster:".$question_content[0]['Name']."</h6>\n";
+        echo "<h6>Time:".$question_content[0]['Time']."</h6>\n";
+		echo "</fieldset>";
+		echo "</div>";
+	
+	
+	?>
+	
+	<div class="container"
+	<?php
+	if((isset($_SESSION['login']))&&($_SESSION['login']==true))
+	{
+	//print_r($_SESSION);
+	}
+	else{
+	
+	echo "hidden='true'";
+	//header("Location: http://localhost/546Final/pages/index.php");
 	}
 	?>
+	>
+
+	<form  role="form" action="../include/addAnswer.php" method="Post"   id ="addAnswer_form" enctype="multipart/form-data" novalidate="novalidate">
+			<fieldset>
+				<legend>Post Your Answer</legend>
+				<div class="form-group">
+					<div class="row">
+						<div class="col-md-4">
+						<input  type="hidden" id="QID" name="QID" value="<?PHP if(isset($_GET['var'])){echo $_GET['var'];} ?>"></input>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="row">
+						<div class="col-md-4">
+							<label for="Content">Content:</label> 
+							 <textarea id="Content" name="Content" rows="5" cols="40"></textarea>
+						</div>
+					</div>
+				</div>
+				<button type="submit" class="btn btn-primary" name="answer_submit" id="answer_submit">Submit</button>
+			</fieldset>
+		</form>
+
+	</div>
+	
+	<div class="container">
+	<fieldset>
+				<legend>Answers</legend>
+		<?PHP
+		$answer_content=GetQuestion_Answer($question_id);
+		//print_r($answer_content[0]);
+		$i=0;
+		$num=count($answer_content);
+		while($i<$num)
+		{
+		echo "<div class=\"jumbotron\">\n";
+		echo "<h6>Answer by:".$answer_content[0]['Name']."</h6>\n";
+        echo "<h6>Time:".$answer_content[0]['Time']."</h6>\n";	
+	    echo "<p><font size=\"3\">".$answer_content[$i]['Content']."</font></p>";
+        echo "</div>";		
+		$i++;
+		}
+	}
+	?>
+</fieldset>
 	</div>
 	<!-- /container -->
 
